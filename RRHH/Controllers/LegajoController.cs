586e30862467546cd47641658a098baa71508c33
Ariel Fabian <arfablumia@hotmail.com>
2023-04-04 09:59:19 -0300

    Varios

--- RRHH/Controllers/LegajoController.cs
+++ RRHH/Controllers/LegajoController.cs
@@ -111,7 +111,7 @@ namespace RRHH.Controllers
             if (HttpContext.Session.GetInt32("ACTIVO_ACTUAL_LEGAJO") != null) activo = (int)HttpContext.Session.GetInt32("ACTIVO_ACTUAL_LEGAJO");
 
 
-            if (perfil_id > 0 && perfil_id <= 3 || perfil_id == 5 || perfil_id <= 6)
+            if (perfil_id > 0 && perfil_id <= 3 || perfil_id == 5 || perfil_id == 6)
             {
                 ILegajoRepo legajoRepo;
 
@@ -228,7 +228,7 @@ namespace RRHH.Controllers
             int? usuario_local_id = HttpContext.Session.GetInt32("USUARIO_LOCAL_ID");
 
 
-            if (perfil_id > 0 && perfil_id <= 3 || perfil_id == 5 || perfil_id <= 6)
+            if (perfil_id > 0 && perfil_id <= 3 || perfil_id == 5 || perfil_id == 6)
             {
                 ILegajoRepo legajoRepo;
 
